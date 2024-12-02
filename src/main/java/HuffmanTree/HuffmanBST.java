47b74b5c9117c8cff7bbc092d4ecb20faa0c783d
VEX <ahmedsherif000035@gmail.com>
2024-12-02 11:34:16 +0200

    Implement getLeaf function to get the corresponding character of a given stream

--- src/main/java/HuffmanTree/HuffmanBST.java
+++ src/main/java/HuffmanTree/HuffmanBST.java
@@ -11,6 +11,19 @@ public class HuffmanBST {
         root = new HuffInternalNode(l, r, wt);
     }
 
+    public static HuffmanBST buildTree() {
+        HuffmanBST tmp1, tmp2, tmp3 = null;
+
+        while (Hheap.heapsize() > 1) { // While two items left
+            tmp1 = Hheap.removemin();
+            tmp2 = Hheap.removemin();
+            tmp3 = new HuffmanBST(tmp1.root(), tmp2.root(),
+                    tmp1.weight() + tmp2.weight());
+            Hheap.insert(tmp3);
+        }
+        return tmp3;
+    }
+
     public HuffBaseNode root() {
         return root;
     }
@@ -28,6 +41,32 @@ public class HuffmanBST {
             return 0;
         } else {
             return 1;
+        }
+    }
+
+    public Character getLeaf(String stream) {
+        return searchValue(root,stream);
+    }
+    private char searchValue(HuffBaseNode node, String path) {
+        if (node == null) {
+            throw new IllegalArgumentException("Path does not exist in the tree.");
+        }
+        if (path.isEmpty()) {
+            if (node.isLeaf()) {
+                HuffLeafNode leaf = (HuffLeafNode) node;
+                return leaf.value(); // Return the character at the leaf
+            } else {
+                throw new IllegalArgumentException("Path does not lead to a leaf node.");
+            }
+        }
+        char direction = path.charAt(0);
+        HuffInternalNode internalNode = (HuffInternalNode) node;
+        if (direction == '0') {
+            return searchValue(internalNode.left(), path.substring(1)); // Go left
+        } else if (direction == '1') {
+            return searchValue(internalNode.right(), path.substring(1)); // Go right
+        } else {
+            throw new IllegalArgumentException("Path contains invalid character: " + direction);
         }
     }
 
@@ -53,18 +92,5 @@ public class HuffmanBST {
             return leftPath;
         }
         return searchLeaf(internalNode.right(), element, path + "1");
-    }
-
-    public static HuffmanBST buildTree() {
-        HuffmanBST tmp1, tmp2, tmp3 = null;
-
-        while (Hheap.heapsize() > 1) { // While two items left
-            tmp1 = Hheap.removemin();
-            tmp2 = Hheap.removemin();
-            tmp3 = new HuffmanBST(tmp1.root(), tmp2.root(),
-                    tmp1.weight() + tmp2.weight());
-            Hheap.insert(tmp3);
-        }
-        return tmp3;
     }
 }
